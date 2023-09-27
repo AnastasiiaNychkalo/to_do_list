8189284a9ccc82f2e576149935d8f8b550a76d34
AnastasiiaNychkalo <95881352+AnastasiiaNychkalo@users.noreply.github.com>
2023-09-27 08:22:27 +0000

    fixed

--- created-list.js
+++ created-list.js
@@ -1,19 +1,19 @@
-const newListNameInput = document.getElementById('created-list__input-list');
-const addListNameBtn = document.getElementById('created-list__new-list');
-const listName = document.getElementById('list_name');
+// const newListNameInput = document.getElementById('created-list__input-list');
+// const addListNameBtn = document.getElementById('created-list__new-list');
+// const listName = document.getElementById('list_name');
 
-addListNameBtn.addEventListener('click', function () {
-    const newOptionValue = newListNameInput.value;
+// addListNameBtn.addEventListener('click', function () {
+//     const newOptionValue = newListNameInput.value;
 
-    if (newOptionValue.trim()) {
-        const formattedValue = newOptionValue.charAt(0).toUpperCase() + newOptionValue.slice(1);
+//     if (newOptionValue.trim()) {
+//         const formattedValue = newOptionValue.charAt(0).toUpperCase() + newOptionValue.slice(1);
 
-        const newOption = document.createElement('option');
-        newOption.value = formattedValue;
-        newOption.textContent = formattedValue;
+//         const newOption = document.createElement('option');
+//         newOption.value = formattedValue;
+//         newOption.textContent = formattedValue;
 
-        listName.appendChild(newOption);
+//         listName.appendChild(newOption);
 
-        newListNameInput.value = '';
-    }
-});
+//         newListNameInput.value = '';
+//     }
+// });
